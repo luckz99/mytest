c5f8632df387c5db263a9a9ade9a9766b99f2883
luckz99 <40238584+luckz99@users.noreply.github.com>
2020-08-27 09:45:17 +0800

    Update hashchain.py

--- hashchain.py
+++ hashchain.py
@@ -4,6 +4,28 @@ from tkinter import filedialog
 root = Tk()
 root.title('Malicious VBA Macro Detection System')
 
+#Initiating the menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+def our_command():
+    pass
+
+#Create a menu item
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New...", command=our_command)
+file_menu.add_separator()
+file_menu.add_command(label="Exit...", command=root.quit)
+
+#create an edit menu
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=our_command)
+edit_menu.add_separator()
+edit_menu.add_command(label="Copy", command=our_command)
+
+
 #Creating a Label Widget
 myLabel1 = Label(root, text="Hello World!\nHello You!\nHello My fren!")
 myLabel2 = Label(root, text="My name is luckzzzzz")
@@ -23,5 +45,7 @@ def open():
 
     
 my_btn = Button(root, text="Open File", command=open).grid(row=0,column=1)
+
+
 
 root.mainloop()
